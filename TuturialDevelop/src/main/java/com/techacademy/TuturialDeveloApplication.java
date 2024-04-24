96689ad418ee9936374362b65fce36525f7dc2cd
dir-FY24sumikuramasaki <MasakiSumikura()@Dirbato-1302x>
2024-04-24 14:34:03 +0900

    初回

---
+++ TuturialDevelop/src/main/java/com/techacademy/TuturialDeveloApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuturialDeveloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuturialDeveloApplication.class, args);
+	}
+
+}
